7cc0a5c53f871f56b3e647b1d8487c1351a4859d
agnieszkasia <agnieszkasia.glinka@gmail.com>
2021-04-19 22:42:50 +0200

    Initialize database

--- app/Controllers/DatabaseController.php
+++ app/Controllers/DatabaseController.php
@@ -75,3 +75,30 @@ function connectToDatabase(): mysqli {
     }
     return $con;
 }
+
+function initialize(){
+    $con = mysqli_connect('localhost','root','');
+
+    $databaseSql = "CREATE DATABASE `systemintegrationproject`;";
+    mysqli_query($con, $databaseSql);
+
+    $init = "CREATE TABLE `systemintegrationproject`.`laptops` (
+    `manufacturer` VARCHAR(255) NOT NULL ,
+    `screen_size` VARCHAR(255) NOT NULL ,
+    `screen_resolution` VARCHAR(255) NULL ,
+    `screen_type` VARCHAR(255) NULL ,
+    `screen_touch` VARCHAR(255) NULL ,
+    `processor_name` VARCHAR(255) NULL ,
+    `processor_physical_cores` VARCHAR(255) NULL ,
+    `processor_clock_speed` VARCHAR(255) NULL ,
+    `ram` VARCHAR(255) NULL ,
+    `disc_type` VARCHAR(255) NULL ,
+    `disc_storage` VARCHAR(255) NULL ,
+    `graphic_card_name` VARCHAR(255) NULL ,
+    `graphic_card_memory` VARCHAR(255) NULL ,
+    `os` VARCHAR(255) NULL ,
+    `disc_reader` VARCHAR(255) NULL ,
+    `id` INT NOT NULL AUTO_INCREMENT ,
+    PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+    mysqli_query($con, $init);
+}
